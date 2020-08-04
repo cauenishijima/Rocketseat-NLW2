28762aee4a13ae8c111ad1276c1175af0e234d61
Cauê Nishijima <caue1987@gmail.com>
2020-08-04 10:08:30 -0300

    Criando conexão com o db sqlite3

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,21 @@
 import express from 'express';
 const app = express();
 
-app.get('/users', (request, response) => {
-    return response.json({
-        name: 'Cauê Nishijma',
-        age: 33
-    })
+app.use(express.json());
+
+// Parametros
+// Corpo (Request Body): Dados para criação ou atualização de um registro
+//
+// Route Params: Identificar qual recurso eu quero atualizar ou deletar. 
+//      ex.: app.delete('/users/:id', ...)
+// E para recuperar o parameto 'id' é só usar: request.params.id
+//
+// Query Params: Usado para paginação, filtros, ordenação.: 
+//      ex.: http://localhost:3001/users?page=2&sort=name
+// E para acessar esses parametros usamos: request.query.page || request.query.sort
+
+app.get('/', (request, response) => {
+    return response.json({message: "Hello World"});
 })
 
-app.listen(3001, () => {
-    console.log('Server running in port 3001')
-})
+app.listen(3001);
