6e0363bb6e54848c69587724b86351368a859c75
Cauê Nishijima <caue1987@gmail.com>
2020-08-09 20:12:23 -0300

    Finalização do frontend já com a integração com o backend

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,34 +3,55 @@ import React from 'react'
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 
 import './styles.css'
+import api from '../../services/api'
 
+export interface Teacher {
+    avatar: string,
+    bio: string,
+    cost: Number,
+    id: string,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
 
-function TeacherItem() {
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('connections', {
+            user_id : teacher.id
+        })
+    }
+
     return (
         <article className='teacher-item'>
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/25585900?s=460&u=3984274f8e4936a9bbce01e253ba1080cb2bbdd4&v=4" alt="Cauê Nishijima" />
+                <img src={teacher.avatar} alt="Cauê Nishijima" />
                 <div>
-                    <strong>Cauê Nishijima</strong>
-                    <span>JavaScript</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta da melhores tecnologias de desenvolvimento web.
-            <br /> <br />
-            Apaixonado por aprender e aplicar novas tecnologias e/ou arquiteturas que ajudam a melhorar a performance dos apps e a legibilidade do código.
-        </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/hora
-                <strong>R$ 90,00</strong>
+                <strong>R$ {teacher.cost}</strong>
                 </p>
-                <button type="button">
+                <a 
+                    target='_blank' 
+                    onClick={createNewConnection} 
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                >
                     <img src={whatsappIcon} alt="Whatsapp" />
-                Entrar em contato
-            </button>
+                    Entrar em contato
+                </a>
             </footer>
         </article>
     )
